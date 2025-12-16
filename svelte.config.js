bc0c3b85b117552122611afb2ee96458818d4779
niko! <200838252+xenon-pl@users.noreply.github.com>
2025-12-16 17:27:12 +0000

    fix shit for pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,15 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'index.html'
+		})
 	}
 };
+
+export default config;
